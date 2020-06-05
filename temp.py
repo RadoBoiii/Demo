cd7226830120edeefe18160dbcf80a9a56d2a37b
RadoBoiii <rishabhsshinde27@gmail.com>
2020-06-05 12:39:06 +0530

    another temp file

---
+++ temp.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import re
+print( sum( [ ****** *** * in **********('[0-9]+',**************************.read()) ] ) )  
